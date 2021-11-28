195c85431125b8525a22114dbf8bb69112833c3d
Brooke <brooke.baxa@gmail.com>
2021-11-28 15:07:29 -0800

    Added server.js, reconfigured, and including json files to run a local server.

---
+++ server.js
@@ -0,0 +1,12 @@
+//dependencies
+const express 	= require('express');
+const app		= express();
+
+
+//middleware
+app.use(express.static('public'));
+
+//listener
+app.listen(process.env.PORT || 2080,()=> {
+	console.log('listening bruh');
+});
