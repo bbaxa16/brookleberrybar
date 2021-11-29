c4e50007a7dbcff2995582547b2f318d2f5568f0
Brooke <brooke.baxa@gmail.com>
2021-11-28 17:39:53 -0800

    resetting server.js to try and fix localhost issue

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 //dependencies
 const express 	= require('express');
 const app		= express();
+const port  	= process.env.PORT || 8000;
 
 //middleware
 app.use(express.static('public'));
 
 //listener
-app.listen(process.env.PORT || 2080,()=> {
+app.listen(port, ()=> {
 	console.log('listening bruh');
 });
